6c27142a6bbb43fd349cf387c991ec01bcf31537
Donatella Tesei <donatella.tesei@gmail.com>
2022-11-03 15:52:54 +0100

    normalized_database

---
+++ social_network.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 03, 2022 at 03:51 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `social network`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+-- Error reading structure for table social network.comment: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;comment&#039;
+-- Error reading data for table social network.comment: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`comment`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `current_workplace_from_to`
+--
+-- Error reading structure for table social network.current_workplace_from_to: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;current_workplace_from_to&#039;
+-- Error reading data for table social network.current_workplace_from_to: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`current_workplace_from_to`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+-- Error reading structure for table social network.education: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;education&#039;
+-- Error reading data for table social network.education: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`education`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `family_members`
+--
+-- Error reading structure for table social network.family_members: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;family_members&#039;
+-- Error reading data for table social network.family_members: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`family_members`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+-- Error reading structure for table social network.image: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;image&#039;
+-- Error reading data for table social network.image: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`image`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+-- Error reading structure for table social network.post: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;post&#039;
+-- Error reading data for table social network.post: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`post`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `previous_workplace_from_to`
+--
+-- Error reading structure for table social network.previous_workplace_from_to: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;previous_workplace_from_to&#039;
+-- Error reading data for table social network.previous_workplace_from_to: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`previous_workplace_from_to`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+-- Error reading structure for table social network.profile: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;profile&#039;
+-- Error reading data for table social network.profile: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`profile`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag`
+--
+-- Error reading structure for table social network.tag: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;tag&#039;
+-- Error reading data for table social network.tag: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`tag`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table social network.user: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;user&#039;
+-- Error reading data for table social network.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_experience`
+--
+-- Error reading structure for table social network.work_experience: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table &#039;work_experience&#039;
+-- Error reading data for table social network.work_experience: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `social network`.`work_experience`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
